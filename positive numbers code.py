b926061b488590a4a070834e839fdc53a9ff3c38
laasya2334 <93214893+laasya2334@users.noreply.github.com>
2021-10-27 20:07:31 +0530

    Add files via upload

---
+++ positive numbers code.py
@@ -0,0 +1,11 @@
+# python program to print positive numbers in a list
+
+#list of numbers
+list1 = [22, -41, 0, 45, -67, 96]
+
+#iteration each numbers in list
+for num in list1:
+
+        #checking condition
+        if num >=0:
+            print(num, end = " ")
